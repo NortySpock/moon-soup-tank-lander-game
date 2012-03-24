19fd63b1937b4a4fcf346bef48eee5cf6b16114f
David Norton <david.norton@cerner.com>
2012-03-24 13:38:02 -0500

    It draws a box

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+black = [ 0, 0, 0]
+white = [255,255,255]
+ 
+# Set the height and width of the screen
+height = 400
+width = 1000
+size=[width,height]
+screen=pygame.display.set_mode(size)
+
